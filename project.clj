a6bc5a8c8624bd08a4b971a97153b4ca29208147
Trevor Bernard <trevor.bernard@userevents.com>
2013-01-01 14:10:23 -0400

    Include profiles for Clojure 1.3 and 1.5.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/userevents/phaser"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.googlecode.disruptor/disruptor "2.10.4"]]
+  :profiles
+  {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.5"]}
   :min-lein-version "2.0.0")

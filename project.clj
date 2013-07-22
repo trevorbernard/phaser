1024a786951d2c28771a5c23a149eb01fd796f01
Ian Bishop <bishop14@gmail.com>
2013-07-22 11:49:10 -0300

    Bumping to 1.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject userevents/phaser "1.1.4"
+(defproject userevents/phaser "1.1.5-SNAPSHOT"
   :description "A Clojure DSL for the LMAX Disruptor"
   :url "https://github.com/userevents/phaser"
   :license {:name "Apache License, Version 2.0"
